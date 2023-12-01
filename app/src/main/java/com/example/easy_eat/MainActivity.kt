5b8cce26a783dd7c9cf94c85f20b1f12a5257a54
Alwi Achmad Alatas <108041947+alwiAchmad@users.noreply.github.com>
2023-12-01 23:42:08 +0700

    Ngubah tampilan sama mulai benerin layouting

--- app/src/main/java/com/example/easy_eat/MainActivity.kt
+++ app/src/main/java/com/example/easy_eat/MainActivity.kt
@@ -4,68 +4,29 @@ package com.example.easy_eat
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var lgEmail: EditText
-    private lateinit var lgPassword: EditText
-    private lateinit var lgLogin: ImageButton
-
-    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lgEmail = findViewById(R.id.lgEmail) // Update the ID to match your layout
-        lgPassword = findViewById(R.id.lgPassword)
-        lgLogin = findViewById(R.id.lgLogin)
+        val btDaftar = findViewById<Button>(R.id.btDaftar)
 
-        firebaseAuth = FirebaseAuth.getInstance()
-
-        lgLogin.setOnClickListener {
-            loginUser()
-        }
-
-        val lgRegister = findViewById<ImageButton>(R.id.lgRegister)
-
-        lgRegister.setOnClickListener {
+        btDaftar.setOnClickListener {
             val intent = Intent(this, Register::class.java)
             startActivity(intent)
         }
-    }
 
-    private fun loginUser() {
-        val email = lgEmail.text.toString().trim()
-        val password = lgPassword.text.toString().trim()
 
-        if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Email dan Password harus diisi", Toast.LENGTH_SHORT).show()
-            return
+        val linkLogin = findViewById<TextView>(R.id.linkLogin)
+        linkLogin.setOnClickListener{
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
         }
-
-        firebaseAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    // Login berhasil
-                    Toast.makeText(this, "Login berhasil", Toast.LENGTH_SHORT).show()
-                    // Tambahan: Pindah ke halaman utama aplikasi setelah login
-                    val intent = Intent(this, Dashboard::class.java)
-                    startActivity(intent)
-                    finish()
-                } else {
-                    Toast.makeText(
-                        this,
-                        "Login gagal. Periksa kembali email dan password",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
     }
 }
